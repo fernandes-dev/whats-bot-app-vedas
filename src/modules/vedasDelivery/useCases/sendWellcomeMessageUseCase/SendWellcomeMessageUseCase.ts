85d3934a8a021e8587a8f606b3f21bf4dafe45b6
Eduardo Fernandes <eduardo.yugan@gmail.com>
2022-06-01 18:51:15 -0400

    fix: function of difference in hours updated

--- src/modules/vedasDelivery/useCases/sendWellcomeMessageUseCase/SendWellcomeMessageUseCase.ts
+++ src/modules/vedasDelivery/useCases/sendWellcomeMessageUseCase/SendWellcomeMessageUseCase.ts
@@ -17,9 +17,13 @@ class SendWellcomeMessageUseCase {
 
       const allMessages = await this.whatsBot.getAllMessagesInChat(newMessage.chatId, true, false);
 
-      const alreadySentWellcomeMessage = allMessages.find(
-        (currentMessage) => currentMessage.content && currentMessage.content.includes(wellcomeMessage.message)
-      );
+      const lastWellcomeMessageIndex = allMessages.reduce((previous, current, currentIndex) => {
+        if (current.content && current.content.includes(wellcomeMessage.message)) return currentIndex;
+        return previous;
+      }, -1);
+
+      const alreadySentWellcomeMessage = allMessages[lastWellcomeMessageIndex];
+      const lastMessage = allMessages[allMessages.length - 1];
 
       if (!alreadySentWellcomeMessage) {
         await this.whatsBot.sendText(newMessage.from, wellcomeMessage.message);
@@ -28,9 +32,15 @@ class SendWellcomeMessageUseCase {
       }
 
       const alreadySentWellcomeMessageDate = new Date(alreadySentWellcomeMessage.timestamp * 1000);
+      const lastMessageDate = new Date(lastMessage.timestamp * 1000);
+
+      const differenceBetweenCurrentDateAndLastMessageDate = differenceInHours(
+        lastMessageDate,
+        alreadySentWellcomeMessageDate
+      );
 
       const shouldResendWellcomeMessage =
-        differenceInHours(new Date(), alreadySentWellcomeMessageDate) >= Number(configs.INTERVALO_EM_HORAS || 24);
+        differenceBetweenCurrentDateAndLastMessageDate >= Number(configs.INTERVALO_EM_HORAS || 24);
 
       if (shouldResendWellcomeMessage) {
         await this.whatsBot.sendText(newMessage.from, wellcomeMessage.message);
